7a0890701d56e4149492024232f50a5f2a80f102
Logan Woolf <loganwoolf@gmail.com>
2022-03-07 19:54:21 -0700

    Complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,29 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.new do |loc|
+  loc.mens_apparel = true
+  loc.womens_apparel = true
+  loc.annual_revenue = 300000
+  loc.name = "Burnaby"
+end
+burnaby.save
+
+richmond = Store.new do |loc|
+  loc.mens_apparel = false
+  loc.womens_apparel = true
+  loc.annual_revenue = 1260000
+  loc.name = "Richmond"
+end
+richmond.save
+
+gastown = Store.new do |loc|
+  loc.mens_apparel = true
+  loc.womens_apparel = false
+  loc.annual_revenue = 190000
+  loc.name = "Gastown"
+end
+gastown.save
+
+puts Store.count
+puts `clear`
