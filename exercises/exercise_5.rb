d83c1f161b5a4a5fdb955c8b76085661587abbc4
Logan Woolf <loganwoolf@gmail.com>
2022-03-07 21:08:33 -0700

    Modify exercise 5 to use average method
    Changed string format to include currency symbol

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,9 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
-puts "Total Revenue: #{Store.sum("annual_revenue")}"
+puts "Total Revenue: $ #{Store.sum("annual_revenue")}"
 
-puts "Average Store Revenue: #{Store.sum("annual_revenue") / Store.count}"
+puts "Average Store Revenue: $ #{Store.average("annual_revenue").to_i}"
 
 puts "Stores Grossing > $1M: #{Store.where('annual_revenue > 1000000').count}"
 
