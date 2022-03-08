603a29acc194c973394c7adeb133d24155f14a6a
Logan Woolf <loganwoolf@gmail.com>
2022-03-07 20:02:18 -0700

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(annual_revenue: 1260050)
+
+@store1.inspect
+puts `clear`
